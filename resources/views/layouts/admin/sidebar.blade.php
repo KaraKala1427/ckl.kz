1150b6bec15153abb32e2c089ace7284f4edcb83
KOPROG <ernarerbol027@gmail.com>
2021-07-01 17:56:34 +0600

    admin adaptive

--- resources/views/layouts/admin/sidebar.blade.php
+++ resources/views/layouts/admin/sidebar.blade.php
@@ -1,6 +1,6 @@
 @auth
     <div class="sidebar sidebar-admin" style="width: 26%;">
-        <div class="sidebar-wrapper ps" style="   background: #0098f0;     width: 422px;">
+        <div class="sidebar-wrapper ps">
             <div class="logo">
                 <a href="javascript:void(0)" class="simple-text logo-mini">
 
