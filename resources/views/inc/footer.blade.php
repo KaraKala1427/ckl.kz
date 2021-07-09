39f2924a782d286dc37f7ac27eb9c8e110856369
Sergey Maxutov <serabass565@gmail.com>
2021-07-09 15:02:03 +0600

    Update footer.blade.php

--- resources/views/inc/footer.blade.php
+++ resources/views/inc/footer.blade.php
@@ -94,7 +94,7 @@
                                 </div>
                             </nav>
                             <div class="copy">
-                                <p>©2020 <span class="text-grey"></span></p>всем привет 123132 </div>
+                                <p>©2020 <span class="text-grey"></span></p> 777 </div>
                         </div>
                     </div>
                     <!-- end inner-grid -->
