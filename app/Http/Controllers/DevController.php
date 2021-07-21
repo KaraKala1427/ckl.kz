8540350faa934b2643c41c65ae5f79d42717a049
KOPROG <ernarerbol027@gmail.com>
2021-07-21 17:02:03 +0600

    link for storage

--- app/Http/Controllers/DevController.php
+++ app/Http/Controllers/DevController.php
@@ -8,9 +8,12 @@ use Illuminate\Support\Facades\Artisan;
 class DevController extends Controller
 {
 
-
-    public function link()
+    public function link(Request $request)
     {
-       return Artisan::call('storage:link');
+        if ($request->get('key') !== '11155') {
+            abort(403);
+        }
+
+        return Artisan::call('storage:link');
     }
 }
