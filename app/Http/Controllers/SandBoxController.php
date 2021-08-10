39cfe3f0603d25f9ae43d25ed02c1affbb7e2c78
Phpshka <56721354+Rustam-LaraCast@users.noreply.github.com>
2021-08-10 16:00:30 +0600

    add users

--- app/Http/Controllers/SandBoxController.php
+++ app/Http/Controllers/SandBoxController.php
@@ -11,29 +11,6 @@ class SandBoxController extends Controller
     public function sandbox(){
 
         User::create([
-            "username" => "super_admin",
-            "password" => bcrypt('password'),
-            "role_id"  =>  Role::SUPER_ADMIN
-        ]);
-
-        User::create([
-            "username" => "admin",
-            "password" => bcrypt('password'),
-            "role_id"  => Role::ADMIN
-        ]);
-
-        User::create([
-            "username" => "yulia",
-            "password" => bcrypt('Asdfgh123#'),
-            "role_id"  => Role::ADMIN
-        ]);
-        User::create([
-            "username" => "aizhan",
-            "password" => bcrypt('Nahzia321!'),
-            "role_id"  => Role::ADMIN
-        ]);
-
-        User::create([
             "username" => "Ruslan",
             "password" => bcrypt('2221904r'),
             "role_id"  => Role::ADMIN
