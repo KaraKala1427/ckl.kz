ec85365b5a6d3e8db3b131a5891b2f762c698fd0
Phpshka <56721354+Rustam-LaraCast@users.noreply.github.com>
2021-08-23 15:02:51 +0600

    js

--- resources/js/scripts/session.js
+++ resources/js/scripts/session.js
@@ -1,14 +1,17 @@
 $(document).ready(function(){
 
+    window.setCityName = function setCityName(city) {
+
+        sessionStorage.setItem('city', city);
+    }
     $('#city-label').html(sessionStorage.getItem('city'));
 });
 
 $(document).ready(function(){
     //console.log(sessionStorage.getItem('city'));
-    if(sessionStorage.getItem('city') !== undefined) {
+    if(sessionStorage.getItem('city') != undefined) {
         $('#city-label').html(sessionStorage.getItem('city'));
     } else {
         $('#city-label').html('Алматы');
     }
-
 });
