7e6bcd5b1b65f6e6c66777baa20d30d082e0c32c
KOPROG <ernarerbol027@gmail.com>
2021-10-19 09:47:02 +0600

    callback mail.blade changed

--- resources/views/email.blade.php
+++ resources/views/email.blade.php
@@ -1,15 +1,12 @@
 <p>Имя : {{ $fullname }}</p>
 <p>Номер : +7{{$phone}}</p>
-<p>Почта : {{$email}}</p>
 <p>Удобное время (время Астаны) : {{$callDate}}</p>
 <p>Позвоните прямо сейчас :
     @if($callNow == "true")
         Да
     @elseif($callNow == "false")
-       Нет
+        Нет
     @endif
 </p>
-<p>Вопрос : {!! $qst !!}</p>
-<p>Со страницы : {{ $frompage }}</p>
 
 
