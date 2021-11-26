4683c3e11f118b3c04cdd51f11c16d19a5f3b7a0
Phpshka <56721354+Rustam-LaraCast@users.noreply.github.com>
2021-11-26 17:38:44 +0600

    covid blade changed

--- resources/views/pages/covid.blade.php
+++ resources/views/pages/covid.blade.php
@@ -25,7 +25,8 @@
                                    type="" name="iin" maxlength="12">
                         </fieldset>
                     </div>
-
+                    <div id="overLoader" class="wheel-loading" style="display: none; ">
+                        <img src="{{ asset('images/ajax-loader.gif') }}" alt="Loading..."></div>
                     <!-- Имя -->
                     <fieldset class="field-set col col--3-12" style="false">
                         <label for="orderfirstName" class="field-set__label checkList">
@@ -68,7 +69,7 @@
 
                     <!--  Номер документа	 -->
 
-                    <fieldset class="field-set col col--4-12" style="false">
+                    <fieldset class="field-set col col--3-12" style="false">
                         <label for="orderDocNumber" class="field-set__label checkList">
                             Номер документа
                         </label>
@@ -77,8 +78,23 @@
                         <div class="small col" id="checkDocNumber1"></div>
                     </fieldset>
 
+
+                    <!-- Тип документа	 -->
+
+                    <fieldset class="field-set col col--3-12" id="fieldDocType1">
+                        <label for="orderDocType" class="field-set__label checkList">
+                            Тип документа </label>
+                        <select
+                            name="docType[]" id="docType1" tabindex="-1" class="docTypes datas agentData1 field">
+                            <option value="v0-empty">--</option>
+                            <option value="1144">Вид на жительство</option>
+                            <option value="1165" selected="">Удостоверение личности гражданина Казахстана</option>
+                            <option value="1156">Паспорт гражданина Казахстана</option>
+                        </select>
+                    </fieldset>
+
                     <!-- Дата выдачи документа -->
-                    <fieldset class="field-set col col--4-12" style="false">
+                    <fieldset class="field-set col col--3-12" style="false">
                         <label for="orderDocDate" class="field-set__label checkList">
                             Дата выдачи документа
                         </label>
@@ -89,14 +105,13 @@
 
                     <!-- Кем выдан	 -->
 
-                    <fieldset class="field-set col col--4-12" style="false">
+                    <fieldset class="field-set col col--3-12" style="false">
                         <label for="orderdocumentGivedBy" class="field-set__label checkList">
                             Кем выдан
                         </label>
                         <input class="field datas keyboardInput agentData1" id="documentGivedBy" type="text"
                                name="documentGivedBy" maxlength="10" value="" autocomplete="off">
                     </fieldset>
-
 
                 </div>
 
@@ -121,35 +136,6 @@
                 </div>
 
 
-                <div class="grid">
-
-                    <!-- Мобильный номер -->
-                    <fieldset class="field-set col col--6-12">
-                        <label for="orderPhone" class="field-set__label checkList">
-                            Мобильный телефон </label>
-                        <input type="tel" class="field interTel datas phone_number" id="phone" name="phone" value="">
-                    </fieldset>
-
-                    <!-- E-Mail -->
-                    <fieldset class="field-set col col--6-12">
-                        <label for="orderEmail" class="field-set__label checkList">
-                            E-Mail </label>
-                        <input type="text" class="field datas keyboardInput agentData1" id="email" name="email"
-                               value="">
-                        <div class="small col" id="checkEmail1"></div>
-                    </fieldset>
-
-
-                    <fieldset class="field-set col col--6-12">
-                        <label for="orderEmail" class="field-set__label checkList">
-                            Страховая сумма</label>
-                        <input type="text" class="field datas keyboardInput agentData1" id="limitSum" name="limitSum"
-                               value="">
-                        <div class="small col" id="checkEmail1"></div>
-                        <p class="small text-grey">Период страхования - 12 месяцев</p>
-                    </fieldset>
-
-                </div>
                 <h3 class="calculator__title">Анкета</h3>
 
                 <div class="grid">
@@ -209,6 +195,87 @@
 
                         </div>
                     </div>
+
+                    <div class="grid">
+
+                        <!--    Страховая сумма -->
+
+                        <fieldset class="field-set col col--6-12">
+                            <label for="orderEmail" class="field-set__label checkList">
+                                Страховая сумма</label>
+                            <input type="text" class="field datas keyboardInput agentData1" id="Calc_Sum"
+                                   name="Calc_Sum"
+                                   value="">
+                            <div class="small col" id="checkEmail1"></div>
+                            <p class="small text-grey">Период страхования - 12 месяцев</p>
+                        </fieldset>
+
+                        <!-- Программа -->
+
+                        <fieldset class="field-set col col--6-12" style="false">
+                            <label for="orderBenefit" class="field-set__label">
+                                Программа</label>
+                            <select
+                                name="benefit[]" id="benefit1" tabindex="-1"
+                                class="benefits datas agentData1 field">
+                                <option value="v0-empty">--</option>
+                                <option value="221721">Прогрмма 1</option>
+                                <option value="221722">Прогрмма 2</option>
+                                <option value="221720">Прогрмма 3</option>
+                            </select>
+                        </fieldset>
+
+
+                        <fieldset class="field-set col col--6-12" style="false">
+                            <h3 class="col">Дата начала договора</h3>
+                            <input class="field field--date edate col--6-12" id="agrBeg" type="tel" name="agrBeg"
+                                   maxlength="10" placeholder="dd.mm.yyyy" value="" autocomplete="off">
+                            <br><span class="small error" style="display: none" id="textAgrBeg"></span>
+                            <input type="hidden" id="ssid" value="939ae3ec9dc28ee495a304aa33396d48">
+                        </fieldset>
+
+                        <fieldset class="field-set col col--6-12" style="false">
+                            <h3 class="col">Дата окончания договора</h3>
+                            <input class="field col--6-12 edate field--date" id="agrEnd" type="tel" name="agrEnd"
+                                   maxlength="10" value="" placeholder="dd.mm.yyyy" autocomplete="off" disabled="">
+                        </fieldset>
+
+
+                        <!-- Мобильный номер -->
+
+                        <fieldset class="field-set col col--6-12">
+                            <label for="orderPhone" class="field-set__label checkList">
+                                Мобильный телефон </label>
+                            <input type="tel" class="field interTel datas phone_number" id="Phone" name="Phone"
+                                   value="">
+                        </fieldset>
+
+                        <!-- E-Mail -->
+                        <fieldset class="field-set col col--6-12">
+                            <label for="orderEmail" class="field-set__label checkList">
+                                E-Mail </label>
+                            <input type="text" class="field datas keyboardInput agentData1" id="Email" name="Email"
+                                   value="">
+                            <div class="small col" id="checkEmail1"></div>
+                        </fieldset>
+
+                    </div>
+
+
+                    <fieldset class="field-set col col--full" style="false">
+                        <label for="orderBenefit" class="field-set__label">
+                            Способ уведомления</label><select
+                            name="benefit[]" id="benefit1" tabindex="-1"
+                            class="benefits datas agentData1 field">
+                            <option value="v0-empty">--</option>
+                            <option value="221721">Email от Коммеска + Email от ЕСБД</option>
+                            <option value="221722">Email от Коммеска + SMS от ЕСБД</option>
+                            <option value="221720">SMS от ЕСБД</option>
+                            <option value="221719">SMS от Коммеска + Email от ЕСБД</option>
+                            <option value="221723">SMS от Коммеска + SMS от ЕСБД</option>
+                        </select>
+                    </fieldset>
+
                     <fieldset class="field-set col col--4-12">
                         <br>
                         <button type="submit" class="button button--hollow agentClear" id="save">Расчитать</button>
@@ -217,6 +284,9 @@
             </form>
         </div>
     </div>
+
+
+
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://unpkg.com/imask"></script>
@@ -228,8 +298,7 @@
                 dateFormat: "dd.mm.yyyy"
             });
             $('.edate').val("");
-
-            $('#covidFrom').submit(function(event){
+            $('#covidForm').submit(function (event) {
                 event.preventDefault();
             });
             var input = document.querySelectorAll('.edate');
@@ -254,7 +323,7 @@
                     }
                 });
             };
-            for (var j = 0; j < 2; j++) {
+            for (var j = 0; j < 3; j++) {
                 dateInputMask(input[j]);
             }
 
@@ -273,7 +342,6 @@
 
         $(".iins").on('keypress change', async function () {
             var iin = $("#iin").val();
-            console.log(iin);
             if (iin.length == 12) {
                 $.ajax({
                     type: 'POST',
@@ -283,8 +351,13 @@
                         iin: iin,
                         _token: '{{csrf_token()}}'
                     },
+                    beforeSend: function () {
+                        $('#overLoader').show();
+                    },
+
                     success: await function (data) {
-                        console.log(data);
+                        $('#overLoader').hide()
+
                         if (data.code == 200) {
                             data = data.client;
                             $("#lastName").val(data.Last_Name)
