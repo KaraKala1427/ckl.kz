21cebd822c911ded9836bcc29313be6ee29221df
Yernar <ernarerbol027@gmail.com>
2022-03-02 10:24:06 +0600

    hiding section o class removejust

--- resources/views/pages/test/failure-payment.blade.php
+++ resources/views/pages/test/failure-payment.blade.php
@@ -4,4 +4,24 @@
     <div>
         <h1>{{__('navbar.failure_payment_halyk')}}</h1>
     </div>
+
+    <style>
+        .removejust {
+            display: none;
+        }
+
+        section.nav-section {
+            display: none;
+        }
+
+        .card-body {
+            display: none;
+        }
+
+        .nav__list {
+            display: none;
+        }
+    </style>
+
+    </main>
 @endsection
--- resources/views/pages/test/not_allowed.blade.php
+++ resources/views/pages/test/not_allowed.blade.php
@@ -4,4 +4,24 @@
     <div>
         <h1>{{__('navbar.not_allowed_covid_epay')}}</h1>
     </div>
+
+    <style>
+        .removejust {
+            display: none;
+        }
+
+        section.nav-section {
+            display: none;
+        }
+
+        .card-body {
+            display: none;
+        }
+
+        .nav__list {
+            display: none;
+        }
+    </style>
+
+    </main>
 @endsection
